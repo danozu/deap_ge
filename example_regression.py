3f274ac9ac94cfed77ce07cb96c842b7170b9220
danozu <allan.danilo1987@gmail.com>
2021-09-20 15:00:39 -0300

    1

---
+++ example_regression.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 27 15:21:08 2021
+
+@author: allan
+"""
+
+#from ponyge2_adapted_files import Grammar, Individual, initialisation_PI_Grow, crossover_onepoint, mutation_int_flip_per_codon
+from ponyge2_adapted_files import Grammar, ge
+import algorithms
+from functions import pdiv, plog, psqrt
+
+from os import path
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from deap import creator, base, tools
+
+import warnings
+warnings.filterwarnings("ignore")
+
+problem = 'pagie1'
+
+if problem == 'pagie1':
+    X_train = np.zeros([2,676], dtype=float)
+    Y_train = np.zeros([676,], dtype=float)
+
+    data_train = pd.read_table(r"datasets/Pagie1_train.txt")
+    for i in range(2):
+        for j in range(676):
+            X_train[i,j] = data_train['x'+ str(i)].iloc[j]
+    for i in range(676):
+        Y_train[i] = data_train['response'].iloc[i]
+
+    X_test = np.zeros([2,10000], dtype=float)
+    Y_test = np.zeros([10000,], dtype=float)
+
+    data_test = pd.read_table(r"datasets/Pagie1_test.txt")
+    for i in range(2):
+        for j in range(10000):
+            X_test[i,j] = data_test['x'+ str(i)].iloc[j]
+    for i in range(10000):
+        Y_test[i] = data_test['response'].iloc[i]
+
+    GRAMMAR_FILE = 'Pagie1.bnf'
+    
+BNF_GRAMMAR = Grammar(path.join("grammars", GRAMMAR_FILE))
+
+def fitness_eval(individual, points):
+    #points = [X, Y]
+    x = points[0]
+    y = points[1]
+    
+    if individual.invalid == True:
+        return np.NaN,
+
+    try:
+        pred = eval(individual.phenotype)
+    except (FloatingPointError, ZeroDivisionError, OverflowError,
+            MemoryError, ValueError):
+        return np.NaN,
+    assert np.isrealobj(pred)
+    
+    try:
+        fitness = np.mean(np.square(y - pred))
+    except (FloatingPointError, ZeroDivisionError, OverflowError,
+            MemoryError, ValueError):
+        fitness = np.NaN
+    
+    return fitness,
+
+toolbox = base.Toolbox()
+
+# define a single objective, minimising fitness strategy:
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+
+creator.create('Individual', ge.Individual, fitness=creator.FitnessMin)
+
+toolbox.register("populationCreator", ge.initialisation_PI_Grow, creator.Individual) 
+
+toolbox.register("evaluate", fitness_eval, points=[X_train, Y_train])
+
+# Tournament selection:
+toolbox.register("select", ge.selTournament, tournsize=7)
+
+# Single-point crossover:
+toolbox.register("mate", ge.crossover_onepoint)
+
+# Flip-int mutation:
+toolbox.register("mutate", ge.mutation_int_flip_per_codon)
+    
+
+POPULATION_SIZE = 20 # 2000
+MAX_GENERATIONS = 2
+P_CROSSOVER = 0.8
+P_MUTATION = 0.01
+ELITE_SIZE = round(0.01*POPULATION_SIZE)
+
+HALL_OF_FAME_SIZE = 1
+MAX_INIT_TREE_DEPTH = 10
+MIN_INIT_TREE_DEPTH = 1
+MAX_TREE_DEPTH = 17
+MAX_WRAPS = 0
+CODON_SIZE = 255
+
+# create initial population (generation 0):
+population = toolbox.populationCreator(size=POPULATION_SIZE, 
+                                        bnf_grammar=BNF_GRAMMAR, 
+                                        min_init_tree_depth=MIN_INIT_TREE_DEPTH,
+                                        max_init_tree_depth=MAX_INIT_TREE_DEPTH,
+                                        max_tree_depth=MAX_TREE_DEPTH, 
+                                        max_wraps=MAX_WRAPS,
+                                        codon_size=CODON_SIZE
+                                        )
+
+# define the hall-of-fame object:
+hof = tools.HallOfFame(HALL_OF_FAME_SIZE)
+
+import math 
+# prepare the statistics object:
+#stats = tools.Statistics(key=lambda ind: ind.fitness.values if math.isnan(ind.fitness.values[0]) else None)#ind.fitness.values != np.inf else None)
+#stats = tools.Statistics(key=lambda ind: ind.fitness.values[0] if not math.isnan(ind.fitness.values[0]) else np.NaN)#ind.fitness.values != np.inf else None)
+stats = tools.Statistics(key=lambda ind: ind.fitness.values if not ind.invalid else (np.NaN,))#ind.fitness.values != np.inf else None)
+stats.register("avg", np.nanmean)
+#stats.register("std", np.nanstd)
+stats.register("min", np.nanmin)
+stats.register("max", np.nanmax)
+
+# perform the Grammatical Evolution flow:
+population, logbook = algorithms.ge_eaSimpleWithElitism(population, toolbox, cxpb=P_CROSSOVER, mutpb=P_MUTATION,
+                                          ngen=MAX_GENERATIONS, elite_size=ELITE_SIZE,
+                                          bnf_grammar=BNF_GRAMMAR, codon_size=CODON_SIZE, 
+                                          max_tree_depth=MAX_TREE_DEPTH, max_wraps=MAX_WRAPS,
+                                          stats=stats, halloffame=hof, verbose=True)
